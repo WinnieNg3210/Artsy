f541d9333ee3e071b2c9569a4c7fee60fce5b12a
Winnie <winnieng3210@gmail.com>
2021-09-06 20:21:39 -0400

    add productsReducer to entities

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
 import usersReducer from "./users_reducer";
+import productsReducer from "./products_reducer";
 
 
 const entitiesReducer = combineReducers({
-  users: usersReducer
+  users: usersReducer,
+  products: productsReducer
 });
 
 export default entitiesReducer;
+
+// will need to git merge for productsReducer to show up on entities
