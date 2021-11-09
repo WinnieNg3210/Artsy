7117b6f29d486f010ab12e0b269dcdc5f430a514
Winnie <winnieng3210@gmail.com>
2021-11-08 20:05:38 -0500

    product show responsive styling done

--- frontend/components/Products/product_show.jsx
+++ frontend/components/Products/product_show.jsx
@@ -132,13 +132,7 @@ class ProductShow extends React.Component {
     return (
       <div className="product-page-container">
         <img src={product.imageUrl} className="product-show-image" />
-        <div className="product-reviews-container">
-          <ReviewIndexContainer productId={this.props.product.id} />
-          <div className="add-btn-container">{addReviewBtn}</div>
-          {createReview}
-        </div>
-
-        <div className="product-right">
+        <div className="product-right product-show-span-row-2">
           {/* <p className="product-seller">PokemonLover</p> */}
           <div className="product-header-container">
             <p className="product-seller margin-bottom">
@@ -260,6 +254,11 @@ class ProductShow extends React.Component {
               </div>
             </div>
           ) : null}
+        </div>
+        <div className="product-reviews-container">
+          <ReviewIndexContainer productId={this.props.product.id} />
+          <div className="add-btn-container">{addReviewBtn}</div>
+          {createReview}
         </div>
       </div>
     );
